26738b8b3a436b188b7b97ef1609f9908ecc8ae6
Lucas <lucas.ar93@gmail.com>
2021-04-07 13:10:57 -0300

    Work on the question renderization on screen

--- javascript-quiz/source/script.js
+++ javascript-quiz/source/script.js
@@ -1,12 +1,11 @@
-let playerName = document.querySelector("#player-name").value;
-let points = 0;
-let questions = [
+const playerName = document.querySelector("#player-name").value;
+const questions = [
   {
     id: 1,
-    question: "Is JavaScript case sensitive?",
+    question: "JavaScript is case sensitive?",
     alternatives: {
-      a: "Yes",
-      b: "No",
+      a: "True",
+      b: "False",
     },
     correct: "a",
   },
@@ -24,9 +23,8 @@ let questions = [
     question:
       "What is the correct syntax for referring to an external script called 'xxx.js'?",
     alternatives: {
-      a: "<script name:xxx.js>",
-      b:
-        "The forEach function don't have to return anything and map return a new array",
+      a: "script name:xxx.js",
+      b: "script src='xxx.js",
     },
     correct: "b",
   },
@@ -35,7 +33,7 @@ let questions = [
     question: "Where is the correct place to insert a JavaScript?",
     alternatives: {
       a: "Both the <head> section and the <body> section are correct",
-      b: "<head>",
+      b: "in the <head> section",
     },
     correct: "a",
   },
@@ -43,8 +41,8 @@ let questions = [
     id: 5,
     question: "Inside which HTML element do we put the JavaScript?",
     alternatives: {
-      a: "<javascript>",
-      b: "<script>",
+      a: "javascript",
+      b: "script",
     },
     correct: "b",
   },
@@ -59,12 +57,41 @@ function startQuiz() {
 }
 
 function renderQuestions() {
-  for (let question of questions) {
-    document.querySelector(
-      "#question-index"
-    ).innerHTML = `Question ${question.id} of 5`;
-    document.querySelector("#question").innerHTML = question.question;
-    document.querySelector("#alternative-a").innerHTML =
-      question.alternatives.a;
+  for (question of questions) {
+    createQuestionElements();
+  }
+}
+
+function createQuestionElements() {
+  index = question.id;
+  const div = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const h3 = document.createElement("h3");
+  const ul = document.createElement("ul");
+  const alternative1 = document.createElement("li");
+  const alternative2 = document.createElement("li");
+  h2.innerHTML = `Question ${question.id} of ${questions.length}`;
+  h3.innerHTML = question.question;
+  alternative1.innerHTML = question.alternatives.a;
+  alternative2.innerHTML = question.alternatives.b;
+  div.append(h2);
+  div.append(h3);
+  div.append(ul);
+  ul.appendChild(alternative1);
+  ul.appendChild(alternative2);
+  document.querySelector(".quiz-area").append(div);
+  alternative1.className = "quiz-alternative";
+  alternative2.className = "quiz-alternative";
+}
+
+function checkAnswer() {
+  const li = document.querySelectorAll(".quiz-alternative");
+  for (question of questions) {
+    addEventListener("click", function () {
+      if (li === question.correct) {
+        console.log("certo");
+      }
+      console.log("nao");
+    });
   }
 }
