a7ea87b21d59d5e291e0fb621e2687c93e8ab8d3
lucas <lucas.ar93@gmail.com>
2021-06-09 21:08:10 -0300

    Change projects font-family to 'Nunito'

--- calculator/resources/javascript/evalMethod.js
+++ calculator/resources/javascript/evalMethod.js
@@ -1,35 +1,35 @@
   const display = document.querySelector(".display");
-  let values = 0;
-  display.innerHTML = values;
+  let displayValue = 0;
+  display.innerHTML = displayValue;
 
   function number(integer) {
-    if(values === 0){
-      values = "";
+    if(displayValue === 0){
+      displayValue = "";
       display.innerHTML = integer.value;
     }
-    values += integer.value;
-    display.innerHTML = values;
+    displayValue += integer.value;
+    display.innerHTML = displayValue;
   }
 
   function operation(signal) {
-    let tempValue = eval(values);
-    values = `${tempValue} ${signal.value} `;
-    display.innerHTML = values;
+    let tempValue = eval(displayValue);
+    displayValue = `${tempValue} ${signal.value} `;
+    display.innerHTML = displayValue;
   }
 
   function clearDisplay() {
-    values = 0;
-    display.innerHTML = values;
+    displayValue = 0;
+    display.innerHTML = displayValue;
   }
 
   function equals() {
-    values = eval(values);
-    display.innerHTML = values;
+    displayValue = eval(displayValue);
+    display.innerHTML = displayValue;
   }
 
   function mathPoint(point) {
-    values += point.value
-    display.innerHTML = values
+    displayValue += point.value
+    display.innerHTML = displayValue
   }
 
   //I know, this isn't the right way to make a calculator, but i tried from the vrey scratch with almost no experience, so after all I'm glad I've done this work!
